abe6a3e229f1ecc7bb859d541ca666706223e62e
won6c <126748292+won6c@users.noreply.github.com>
2024-09-05 15:03:36 +0900

    Update common.php

--- common.php
+++ common.php
@@ -1,14 +1,14 @@
 <?
     header("Content-Type: text/html; charset=UTF-8;");
 
-    $tb_name = "insecure_board";
-    $upload_path = "upload";
+    $tb_name = "<your db table name>";
+    $upload_path = "<your upload path>";
     
     function mysql_conn(){
         $host = "127.0.0.1";
-        $id = "root";
-        $pw = "apmsetup";
-        $db = "pbl_4";
+        $id = "<your mysql user>";
+        $pw = "<your mysql password>";
+        $db = "<your db name>";
 
         $db_conn = new mysqli($host,$id,$pw,$db);
 
